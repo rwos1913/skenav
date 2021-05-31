0b1c665225aed3cc93a9e155a6a154b3e4dcb9d5
Nops <fake@email.com>
2021-05-31 13:54:08 -0400

    adding imageDirectory flag from the comfig

---
+++ src/main/java/skenav/code/views/UploadView.java
@@ -0,0 +1,9 @@
+package skenav.code.views;
+
+import io.dropwizard.views.View;
+
+public class UploadView extends View {
+    public UploadView(String imageDirectory) {
+        super("pageUpload");
+    }
+}
